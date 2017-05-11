7c1dd9328dc8b250bb417aaa12fce3e90c2d3b9c
Thomas Flucke <tflucke@users.noreply.github.com>
2017-05-10 18:04:25 -0700

    Readded Event model to server

---
+++ server/app/models/Event.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.util.Date;
+
+public class Event
+{
+    public long eventId;
+    public long userId;
+    public String name;
+    public Date time;
+    public String foodType;
+    public String description;
+    public double lat;
+    public double lng;
+}
