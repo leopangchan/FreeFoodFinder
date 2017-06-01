a3c33bf85007d60ed2cfe45abab0968e6c98ee90
Zachary Hatton <zachattackhatton@gmail.com>
2017-05-31 17:00:05 -0700

    Revised Create Event Screen

--- app/src/main/java/com/example/yiupang/freefoodfinder/CreateEventScreen.java
+++ app/src/main/java/com/example/yiupang/freefoodfinder/CreateEventScreen.java
@@ -1,7 +1,7 @@
 package com.example.yiupang.freefoodfinder;
 
 /**
- * Created by Acer on 5/26/2017.
+ * Created by Zachary Hatton on 5/26/2017.
  */
 
 import android.content.Intent;
@@ -13,20 +13,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
+import android.support.v4.app.Fragment;
 
 
 import java.util.ArrayList;
 
-public class CreateEventScreen extends android.support.v4.app.Fragment{
+public class CreateEventScreen extends Fragment{
 
-    @Nullable
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.create_event, container, false);
-        Event newEvent = new Event();
+        //Event newEvent = new Event();
 
-        EditText editText = (EditText) view.findViewById(R.id.box1);
+        /* EditText editText = (EditText) view.findViewById(R.id.box1);
         editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -35,13 +37,11 @@ public class CreateEventScreen extends android.support.v4.app.Fragment{
                 handled = true;
                 return handled;
             }
-        });
+        }); */
+
+        //Button button = (Button) view.findViewById(R.id.button_create);
+        //onClickListener(view);
 
         return view;
     }
-
-
-
-
-
 }
