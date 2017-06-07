2655abd0175025ed9f4f8ab12c168006648a1368
Fernando Diaz <fdiazpai@calpoly.edu>
2017-06-06 20:09:07 -0700

    Test suite for map.

---
+++ app/src/test/java/com/example/yiupang/freefoodfinder/TestMapSuite.java
@@ -0,0 +1,15 @@
+package com.example.yiupang.freefoodfinder;
+
+/**
+ * Created by Fernando on 6/6/17.
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestMapScreenForLoop.class, TestMapScreenWhileLoop.class})
+public class TestMapSuite
+{
+
+}
